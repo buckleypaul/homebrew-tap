38c128577c7fa51e94a629a6a143837b506b092a
Paul Buckley <paul@hubble.com>
2026-01-28 09:56:47 -0800

    Update blescan SHA256

--- Formula/blescan.rb
+++ Formula/blescan.rb
@@ -2,7 +2,7 @@ class Blescan < Formula
   desc "Terminal UI application for scanning and analyzing BLE device advertisements"
   homepage "https://github.com/buckleypaul/blescan"
   url "https://github.com/buckleypaul/blescan/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f2af1d83caed7e77965813dd1edfa29b042ac3d1688555fb22db7ad47bd3386e"
+  sha256 "2778b67db0f9d6e49ca8e8c1f8e465674728e8d2445212a3c8650361002e303b"
   license "MIT"
 
   depends_on "go" => :build
