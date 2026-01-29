cc98bb65c0f29d38c6a62d21aba4922396aaf61f
Paul Buckley <paul@hubble.com>
2026-01-28 16:00:56 -0800

    hubcli: update to v0.2.0

--- Formula/hubcli.rb
+++ Formula/hubcli.rb
@@ -1,8 +1,8 @@
 class Hubcli < Formula
   desc "Terminal UI application for Hubble Network device management"
   homepage "https://github.com/buckleypaul/hubcli"
-  url "https://github.com/buckleypaul/hubcli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "cbacf8bb0835b6962887f3b1d3d5a163f7c27de5c5d8236f2984e270cd34fd49"
+  url "https://github.com/buckleypaul/hubcli/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2b48dc37befbec7b7bc8ca2ece7e61f7cda752dbf6fb30d89b5c86351ddfae0b"
   license "MIT"
 
   depends_on "go" => :build
